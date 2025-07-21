739568c758a05b9ffd2f02e65ac701e94a9fa3e5
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-07-21 19:36:27 +0530

    Update logo.jsx

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-2xl font-bold">Tidal<span className="opacity-50">Tunes</span></h1>
+                <h1 className="text-2xl font-bold">Tidal <span className="opacity-50">Tunes™</span></h1>
             </div>
         </Link>
     )
