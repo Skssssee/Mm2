2ba7fb070dccd49bd88ba16f6babb4f8e390ae90
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-22 11:59:13 +0530

    Update logo.jsx

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-2xl font-bold">Yorsa <span className="opacity-50">Tunes</span></h1>
+                <h1 className="text-2xl font-bold">𝐍ᴏʙɪᴛᴀ <span className="opacity-50">𝐌ᴜsɪᴄ</span></h1>
             </div>
         </Link>
     )
